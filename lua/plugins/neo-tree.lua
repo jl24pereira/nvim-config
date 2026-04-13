9ad6e5ae556cb570053a774e1176ba53f46ce9fb
jlpereir <jlpereir@bancoagricola.com.sv>
2026-04-13 12:10:12 -0600

    Se activa en neto-tree la configuracion para ver archivos .gitignore

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -210,7 +210,10 @@ return {
                     hide_gitignored = true,
                     hide_ignored = true, -- hide files that are ignored by other gitignore-like files
                     -- other gitignore-like files, in descending order of precedence.
-                    ignore_files = {".neotreeignore", ".ignore" -- ".rgignore"
+                    ignore_files = {
+                        ".neotreeignore", 
+                        -- ".ignore" 
+                        -- ".rgignore"
                     },
                     hide_hidden = true, -- only works on Windows for hidden files/directories
                     hide_by_name = {
@@ -221,10 +224,11 @@ return {
                         -- "*/src/*/tsconfig.json",
                     },
                     always_show = { -- remains visible even if other settings would normally hide it
-                        -- ".gitignored",
+                        ".gitignored",
+                        ".gitignore",
                     },
                     always_show_by_pattern = { -- uses glob style patterns
-                        -- ".env*",
+                        ".env*",
                     },
                     never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
                         -- ".DS_Store",
